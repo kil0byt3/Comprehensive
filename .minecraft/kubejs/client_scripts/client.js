88b619b6ee1a6208625c3f3a9df4814d2e1c4be2
kilo <39413400+kil0byt3@users.noreply.github.com>
2024-05-03 20:20:44 -0700

    started scripts, rebalance backpack and hide items

---
+++ .minecraft/kubejs/client_scripts/client.js
@@ -0,0 +1,18 @@
+// priority: 0
+
+// Visit the wiki for more info - https://kubejs.com/
+
+REIEvents.hide('item', event => {
+    event.hide('simplyswords:iron_spear' )
+    event.hide('simplyswords:gold_spear' )
+    event.hide('simplyswords:diamond_spear' )
+    event.hide('simplyswords:netherite_spear' )
+    event.hide('simplyswords:runic_spear' )
+    event.hide('sophisticatedbackpacks:chipped/botanist_workbench_upgrade' )
+    event.hide('sophisticatedbackpacks:chipped/glassblower_upgrade' )
+    event.hide('sophisticatedbackpacks:chipped/carpenters_table_upgrade' )
+    event.hide('sophisticatedbackpacks:chipped/loom_table_upgrade' )
+    event.hide('sophisticatedbackpacks:chipped/mason_table_upgrade' )
+    event.hide('sophisticatedbackpacks:chipped/alchemy_bench_upgrade' )
+    event.hide('sophisticatedbackpacks:chipped/tinkering_table_upgrade' )
+})
---
+++ .minecraft/kubejs/server_scripts/server.js
@@ -0,0 +1,62 @@
+// priority: 0
+
+// Visit the wiki for more info - https://kubejs.com/
+
+console.info('Loaded KiloScript')
+
+ServerEvents.recipes(event => {
+
+
+// Move spears to Archery (better animation)
+
+event.remove({ output: 'simplyswords:iron_spear' })
+event.remove({ output: 'simplyswords:gold_spear' })
+event.remove({ output: 'simplyswords:diamond_spear' })
+event.remove({ output: 'simplyswords:netherite_spear' })
+event.remove({ output: 'simplyswords:runic_spear' })
+
+// Hide broken chipped upgrades
+event.remove({ output: 'sophisticatedbackpacks:chipped/botanist_workbench_upgrade' })
+event.remove({ output: 'sophisticatedbackpacks:chipped/glassblower_upgrade' })
+event.remove({ output: 'sophisticatedbackpacks:chipped/carpenters_table_upgrade' })
+event.remove({ output: 'sophisticatedbackpacks:chipped/loom_table_upgrade' })
+event.remove({ output: 'sophisticatedbackpacks:chipped/mason_table_upgrade' })
+event.remove({ output: 'sophisticatedbackpacks:chipped/alchemy_bench_upgrade' })
+event.remove({ output: 'sophisticatedbackpacks:chipped/tinkering_table_upgrade' })
+
+
+// Sophisticated Backpacks rebalance
+event.remove({ output: 'sophisticatedbackpacks:iron_backpack' })
+event.shaped('sophisticatedbackpacks:iron_backpack', [// arg 1: output
+    'BBB', 
+    'BAB', // arg 2: the shape (array of strings)
+    'BBB'  
+  ], {
+    A: 'sophisticatedbackpacks:backpack',  //arg 3: the mapping object
+    B: 'minecraft:iron_block'
+  }
+)
+
+event.remove({ output: 'sophisticatedbackpacks:gold_backpack' })
+event.shaped('sophisticatedbackpacks:gold_backpack', [// arg 1: output
+    'BBB', 
+    'BAB', // arg 2: the shape (array of strings)
+    'BBB'  
+  ], {
+    A: 'sophisticatedbackpacks:iron_backpack',  //arg 3: the mapping object
+    B: 'minecraft:gold_block'
+  }
+)
+
+event.remove({ output: 'sophisticatedbackpacks:diamond_backpack' })
+event.shaped('sophisticatedbackpacks:diamond_backpack', [// arg 1: output
+    'BBB', 
+    'BAB', // arg 2: the shape (array of strings)
+    'BBB'  
+  ], {
+    A: 'sophisticatedbackpacks:gold_backpack',  //arg 3: the mapping object
+    B: 'minecraft:diamond_block'
+  }
+)
+
+})
