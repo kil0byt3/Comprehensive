e7a372a568dfb93788a800aa5e09180b9f1baa89
kilo <39413400+kil0byt3@users.noreply.github.com>
2024-07-05 11:34:08 -0700

    stacksize and anvils cfg

--- .minecraft/kubejs/startup_scripts/startup.js
+++ .minecraft/kubejs/startup_scripts/startup.js
@@ -9,3 +9,15 @@ StartupEvents.registry('item', e => {
     // If you want a custom item model, you can create one in Blockbench and put it in kubejs/assets/kubejs/models/item/test_item.json
     e.create('comprehensive:arcane_powder').texture('comprehensive:item/arcane_powder').displayName('Arcane Powder')
   })
+
+  ItemEvents.modification(event => {
+    event.modify('#forge:raw_materials', item => {
+      item.maxStackSize = 16
+    })
+    event.modify('#c:ores', item => {
+      item.maxStackSize = 16
+    })
+    event.modify('#c:raw_ores', item => {
+      item.maxStackSize = 16
+    })
+  })
